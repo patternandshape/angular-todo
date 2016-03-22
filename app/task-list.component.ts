a169f280ce22d2b0d7ab6d39577e0b8bdf88325c
Lauryn Davis and Molly Curtin <lryndavis@gmail.com>
2016-03-22 16:22:18 -0700

    add stateful pipes to effect our change detection cycle

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,30 +1,50 @@
-import {Component, EventEmitter} from 'angular2/core';
-import {TaskComponent} from './task.component';
-import {Task} from './task.model';
+import { Component, EventEmitter } from 'angular2/core';
+import { TaskComponent } from './task.component';
+import { Task } from './task.model';
+import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { NewTaskComponent} from './new-task.component';
+import {DonePipe} from './done.pipe';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  directives: [TaskComponent],
+  pipes: [DonePipe],
+  directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   template: `
-  <task-display *ngFor="#currentTask of taskList"
+    <select (change)="onChange($event.target.value)" class="filter">
+      <option value="all">Show All</option>
+      <option value="done">Show Done</option>
+      <option value="notDone" selected="selected">Show Not Done</option>
+    </select>
+    <task-display *ngFor="#currentTask of taskList | done:filterDone"
     (click)="taskClicked(currentTask)"
     [class.selected]="currentTask === selectedTask"
     [task]="currentTask">
-  </task-display>
+    </task-display>
+    <edit-task-details *ngIf="selectedTask" [task]="selectedTask">
+    </edit-task-details>
+    <new-task (onSubmitNewTask)="createTask($event)"></new-task>
   `
 })
 export class TaskListComponent {
-  public taskList: Task[]; // Array<Task>
+  public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
-  constructor (){
+  public filterDone: string = "notDone";
+  constructor() {
     this.onTaskSelect = new EventEmitter();
   }
   taskClicked(clickedTask: Task): void {
-    console.log('child', clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
+  }
+  createTask(description: string): void{
+    this.taskList.push(
+      new Task(description, this.taskList.length)
+    );
+  }
+  onChange(filterOption){
+    this.filterDone = filterOption;
   }
 }
