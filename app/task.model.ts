36550a8b303a8ec8e593494d827983f50aa9016b
Molly Curtin and Oskar Radon <patternandshape@gmail.com>
2016-03-23 13:49:05 -0700

    add comment

--- app/task.model.ts
+++ app/task.model.ts
@@ -1,6 +1,6 @@
 //defines the object our program is built around
 export class Task {
-  // whether the tick-box has been ticked or not
+  // whether the tick-box has been ticked or not, this is outside the Constructor because we declare a starting value for it
   public done: boolean = false;
   // properties every task will have and gives it a number id
   constructor(public description: string, public id: number) {
